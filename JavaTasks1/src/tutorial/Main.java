9ed98411bdce81a7fa8c9454babc2593f305f6a2
Richard Craggs <rcraggs@gmail.com>
2017-04-13 11:23:08 +0100

    tidying code

--- JavaTasks1/src/tutorial/Main.java
+++ JavaTasks1/src/tutorial/Main.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.*;
 import javafx.geometry.Insets;
 
@@ -17,12 +17,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 
 		// Create the vbox which is a expanding vertical container for our controls
-		VBox vbox = new VBox();
+		HBox vbox = new HBox();
 		vbox.setPadding(new Insets(10,10,10,10));
 		vbox.setSpacing(10);
 		
 		// Create a scene of the vbox and add it to the stage
-		Scene scene = new Scene(vbox,400,400);		
+		Scene scene = new Scene(vbox,400,150);		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
@@ -33,11 +33,20 @@ public class Main extends Application {
 		vbox.getChildren().add(new TextField(""));
 		vbox.getChildren().add(setTaskButton);
 		vbox.getChildren().add(taskLabel);
+	
+		{
+			String inScope = "Hello";
+			System.out.println("In Scope" + inScope);			
+		}
 		
-		setTaskButton.setOnAction(e -> { taskLabel.setText("The task is updated at " + Calendar.getInstance().getTime());} );
+		
+		setTaskButton.setOnAction(e -> { 
+			taskLabel.setText("The task is updated at " + Calendar.getInstance().getTime());
+		} );
 	}
 	
 	public static void main(String[] args) {
 		launch(args); // Q? What is launch
 	}
+	
 }
